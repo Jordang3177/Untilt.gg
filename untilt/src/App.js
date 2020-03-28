e463bddd5922986c08f3eb9ac2fb9aec4452f027
Jordan Graves <Jordang3177@gmail.com>
2020-03-29 02:36:46 +0800

    Initial Commit

---
+++ untilt/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import Untilt from "./containers/Untilt/Untilt";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Untilt></Untilt>
+      </div>
+    );
+  }
+}
+
+export default App;
