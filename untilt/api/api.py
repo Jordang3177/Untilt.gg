c11c14e10843a97127a6615db1441244c6754882
Jordan Graves <Jordang3177@gmail.com>
2020-03-30 01:28:35 +0800

    Renamed functions and variables for readability

--- untilt/api/api.py
+++ untilt/api/api.py
@@ -7,14 +7,22 @@ app = Flask(__name__)
 api = ApiKey()
 API_KEY = api.getKey()
 summoner_name_url = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'
-match_url = 'https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/'
+match_history_url = 'https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/'
+match_url = 'https://na1.api.riotgames.com/lol/match/v4/matches/'
 @app.route('/summoner/<summonerName>')
-def get_summoner_name(summonerName):
-    r = requests.get(summoner_name_url + summonerName + '?api_key=' + API_KEY)
-    local_time = get_time_of_game(r.json()['accountId'])
-    return local_time
+def main(summonerName):
+    summoner_data = get_summoner_data(summonerName)
+    match_history = get_match_history(summoner_data.json()['accountId'])
+    wins_by_time = {}
+    for i in range(23):
+        wins_by_time[i] = 0
+    return match_history
 
-def get_time_of_game(accountId):
-    r = requests.get(match_url + accountId + '?api_key=' + API_KEY)
-    epoch = r.json()
-    return epoch
+def get_summoner_data(summoner):
+    response = requests.get(summoner_name_url + summoner + '?api_key=' + API_KEY)
+    return response
+
+def get_match_history(accountId):
+    response = requests.get(match_history_url + accountId + '?api_key=' + API_KEY)
+    epoch_time = response.json()
+    return epoch_time
