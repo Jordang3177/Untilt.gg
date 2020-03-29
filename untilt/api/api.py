4b3b79901bc398e9f442341b8548457bc5f3026e
Jordan Graves <Jordang3177@gmail.com>
2020-03-29 14:30:55 +0800

    Hiding API Key

--- untilt/api/api.py
+++ untilt/api/api.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from api_key import ApiKey
 import requests
 
 app = Flask(__name__)
-
-api_key = 'RGAPI-31e61257-c9a7-4588-907d-acc167ccc544'
+api = ApiKey()
+API_KEY = api.getKey()
 url = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'
 @app.route('/summoner/<summonerName>')
 def get_summoner_name(summonerName):
-    r = requests.get('https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + summonerName + '?api_key=RGAPI-31e61257-c9a7-4588-907d-acc167ccc544')
+    r = requests.get('https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + summonerName + '?api_key=' + API_KEY)
     return r.json()
